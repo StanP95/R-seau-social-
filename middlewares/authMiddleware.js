af60cce17c51f9a69dadca7c8ecd24850760932c
StanP95 <prosperestanley3@gmail.com>
2024-12-15 18:24:11 +0100

    fichier terminé

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -3,10 +3,12 @@ const User = require('../models/user');
 
 exports.protect = async (req, res, next) => {
     const token = req.header('Authorization');
+    const extractedToken = token.startsWith('Bearer ') ? token.split(' ')[1] : token;
     if (!token) return res.status(401).json({ error: 'Access denied, no token provided' });
 
+
     try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const decoded = jwt.verify(extractedToken, process.env.JWT_SECRET);
         req.user = await User.findById(decoded.id);
         next();
     } catch (error) {
