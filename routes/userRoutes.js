b4717d2619de7335ebe472cf3910f1a6a94c8da2
StanP95 <prosperestanley3@gmail.com>
2024-12-15 18:22:37 +0100

    fichier terminé

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { getUser, followUser, unfollowUser } = require('../controllers/userControllers');
+const { getUserById, getUser, followUser, unfollowUser } = require('../controllers/userControllers');
 const { protect } = require('../middlewares/authMiddleware');
 const router = express.Router();
 
-router.get('/:id', getUser);
+router.get('/:id', getUserById);
+router.get('/', getUser);
 router.post('/:id/follow', protect, followUser);
 router.post('/:id/unfollow', protect, unfollowUser);
 
