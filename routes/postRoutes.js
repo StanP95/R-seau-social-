873d448f3c23d0a09bab505a923edc354683f7e4
StanP95 <prosperestanley3@gmail.com>
2024-12-15 18:21:11 +0100

    fichier terminé

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { createPost, getPosts, likePost } = require('../controllers/postControllers');
+const { createPost, getPosts, likePost, deletePost, updatedPost} = require('../controllers/postControllers');
 const { protect } = require('../middlewares/authMiddleware');
 const router = express.Router();
 
 router.post('/', protect, createPost);
+router.delete('/:id', protect, deletePost);
+router.put('/:id', protect, updatedPost)
 router.get('/', getPosts);
 router.post('/:id/like', protect, likePost);
 
