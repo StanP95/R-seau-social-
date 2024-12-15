44d78c15bb9d649c568e3634b0b17689f55a70fc
StanP95 <prosperestanley3@gmail.com>
2024-12-15 18:25:24 +0100

    fichier terminé

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -32,3 +32,33 @@ exports.likePost = async (req, res) => {
         res.status(500).json({ error: error.message });
     }
 };
+
+// Mise à jour d'un post existant
+exports.updatedPost =  async (req, res) => {
+    try {
+      const post = await Post.findById(req.params.id);
+      if (post.userId.toString() === req.body.userId) {
+        const updatedPost = await Post.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true });
+        res.status(200).json(updatedPost);
+      } else {
+        res.status(403).json({ message: "Vous ne pouvez modifier que vos propres posts." });
+      }
+    } catch (err) {
+      res.status(500).json({ message: "Erreur lors de la mise à jour.", error: err.message });
+    }
+  };
+  
+  // Suppression d'un post existant
+  exports.deletePost = async (req, res) => {
+    try {
+      const post = await Post.findById(req.params.id);
+      if (post.userId.toString() === req.body.userId) {
+        await post.deleteOne();
+        res.status(200).json({ message: "Post supprimé avec succès." });
+      } else {
+        res.status(403).json({ message: "Vous ne pouvez supprimer que vos propres posts." });
+      }
+    } catch (err) {
+      res.status(500).json({ message: "Erreur lors de la suppression.", error: err.message });
+    }
+  };
